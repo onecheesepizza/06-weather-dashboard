3a34a879cac91df8898d59063c1708f0f42397c4
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-07-23 00:21:53 -0700

    implement lambda functions

---
+++ functions/current-conditions.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+    const { city } = event.queryStringParameters;
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + process.env.OPENWEATHER_API_KEY;
+    return axios
+        .get(queryURL)
+        .then( res => {
+            return ({
+            statusCode:200 , 
+            body: JSON.stringify(res.data)
+        })})
+        .catch( err => ({ 
+            statusCode: 500, 
+            body: err.toString() 
+        }));
+}
