3c6c2d2a55284041aa70833861e750c0dba44600
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-07 21:19:03 -0700

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i s/OPENWEATHER_API_KEY_REPLACE/${OPENWEATHER_API_KEY}/g netlify.toml &&
-sed -i s/UNSPLASH_API_KEY_REPLACE/${UNSPLASH_API_KEY}/g netlify.toml &&
+sed -i "s|OPENWEATHER_API_KEY_REPLACE|${OPENWEATHER_API_KEY}|" netlify.toml &&
+sed -i "s|UNSPLASH_API_KEY_REPLACE|${UNSPLASH_API_KEY}|" netlify.toml
 echo "inserted environment variables to netlify.toml"
