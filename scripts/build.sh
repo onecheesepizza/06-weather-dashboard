875c0e50b9971a824203c8c3de09cc2577a575dd
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-07 19:35:11 -0700

    add build script log

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 sed -i s/OPENWEATHER_API_KEY_REPLACE/${OPENWEATHER_API_KEY}/g netlify.toml &&
 sed -i s/UNSPLASH_API_KEY_REPLACE/${UNSPLASH_API_KEY}/g netlify.toml &&
-echo [[ -v OPENWEATHER_API_KEY ]];
-echo [[ -v UNSPLASH_API_KEY ]];
+if [[ -v OPENWEATHER_API_KEY ]]
+    echo "OPENWEATHER_API_KEY set"
+else
+    echo "OPENWEATHER_API_KEY NOT set"
+fi
+if [[ -v OPENWEATHER_API_KEY ]]
+    echo "UNSPLASH_API_KEY set"
+else
+    echo "UNSPLASH_API_KEY NOT set"
+fi
 echo "inserted environment variables to netlify.toml"
