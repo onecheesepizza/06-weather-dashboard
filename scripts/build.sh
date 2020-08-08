b698e3b6680fab5394551b59c4a99e0f9b62b24a
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-07 19:24:57 -0700

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i s/OPENWEATHER_API_KEY_REPLACE/${process.env.OPENWEATHER_API_KEY}/g ./netlify.toml &&
-sed -i s/UNSPLASH_API_KEY_REPLACE/${process.env.UNSPLASH_API_KEY}/g ./netlify.toml
+sed -i s/OPENWEATHER_API_KEY_REPLACE/${OPENWEATHER_API_KEY}/g netlify.toml &&
+sed -i s/UNSPLASH_API_KEY_REPLACE/${UNSPLASH_API_KEY}/g netlify.toml
