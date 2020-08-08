f0ce7adc66778454f8e57c8993c2466e3b21cb51
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-07 19:56:16 -0700

    revert build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-sed -i s/OPENWEATHER_API_KEY_REPLACE/$OPENWEATHER_API_KEY/g netlify.toml &&
-sed -i s/UNSPLASH_API_KEY_REPLACE/$UNSPLASH_API_KEY/g netlify.toml &&
-if [[ -v OPENWEATHER_API_KEY ]]
-then
-    echo "OPENWEATHER_API_KEY set"
-    echo $OPENWEATHER_API_KEY
-else
-    echo "OPENWEATHER_API_KEY NOT set"
-fi
-if [[ -v OPENWEATHER_API_KEY ]]
-then
-    echo "UNSPLASH_API_KEY set"
-    echo $UNSPLASH_API_KEY
-else
-    echo "UNSPLASH_API_KEY NOT set"
-fi
+sed -i s/OPENWEATHER_API_KEY_REPLACE/${OPENWEATHER_API_KEY}/g netlify.toml &&
+sed -i s/UNSPLASH_API_KEY_REPLACE/${UNSPLASH_API_KEY}/g netlify.toml &&
 echo "inserted environment variables to netlify.toml"
-echo "branch"
-echo $BRANCH
