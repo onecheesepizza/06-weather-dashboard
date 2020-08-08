2c0d86e55c2c5430879897418b8b01e825f43fd5
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-07 19:48:19 -0700

    fix var declarations

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-sed -i s/OPENWEATHER_API_KEY_REPLACE/${OPENWEATHER_API_KEY}/g netlify.toml &&
-sed -i s/UNSPLASH_API_KEY_REPLACE/${UNSPLASH_API_KEY}/g netlify.toml &&
+sed -i s/OPENWEATHER_API_KEY_REPLACE/$OPENWEATHER_API_KEY/g netlify.toml &&
+sed -i s/UNSPLASH_API_KEY_REPLACE/$UNSPLASH_API_KEY/g netlify.toml &&
 if [[ -v OPENWEATHER_API_KEY ]]
 then
     echo "OPENWEATHER_API_KEY set"
-    echo ${OPENWEATHER_API_KEY}
+    echo $OPENWEATHER_API_KEY
 else
     echo "OPENWEATHER_API_KEY NOT set"
 fi
 if [[ -v OPENWEATHER_API_KEY ]]
 then
     echo "UNSPLASH_API_KEY set"
-    echo ${UNSPLASH_API_KEY}
+    echo $UNSPLASH_API_KEY
 else
     echo "UNSPLASH_API_KEY NOT set"
 fi
 echo "inserted environment variables to netlify.toml"
 echo "branch"
-echo ${BRANCH}
+echo $BRANCH
